0fb6fcef82e62e1b14abcac2be6ec0b670882b63
Mateusz Lewartowski <lewar2013c@gmail.com>
2020-07-30 17:24:31 +0200

    request api, used api nbp with fs require, saved to currencies.txt etc..

--- app.js
+++ app.js
@@ -15,38 +15,68 @@ const fetch = require('node-fetch');
 
 // `http://numbersapi.com/${number}/${type}?json`
 
-const arg = process.argv[2];
-let type = "";
+// const arg = process.argv[2];
+// let type = "";
 
-if (arg.indexOf('--year') === 0) {
-  console.log('looking year....');
-  type = "year";
-} else if (arg.indexOf('--math') === 0) {
-  console.log('looking info about number...')
-  type = "math";
-} else if (arg.indexOf('--trivia') === 0) {
-  console.log('looking info about trivia....')
-  type = "trivia";
-}
-
-const equalSign = arg.search('=');
-
-if (equalSign === -1) console.log('you not put a number!');
-
-const number = arg.slice(equalSign + 1) || 1900;
-
-// if (number === '' || isNaN(Number(number))) {
-//   console.log('its not a number!');
-//   process.exit();
+// if (arg.indexOf('--year') === 0) {
+//   console.log('looking year....');
+//   type = "year";
+// } else if (arg.indexOf('--math') === 0) {
+//   console.log('looking info about number...')
+//   type = "math";
+// } else if (arg.indexOf('--trivia') === 0) {
+//   console.log('looking info about trivia....')
+//   type = "trivia";
 // }
 
-fetch(`http://numbersapi.com/${number}/${type}?json`)
-  .then(response => {
-    if (response.ok) {
-      return response.json()
-    } else {
-      throw new Error('ohhhh something wrong: ' + response.status)
-    }
+// const equalSign = arg.search('=');
+
+// if (equalSign === -1) console.log('you not put a number!');
+
+// const number = arg.slice(equalSign + 1) || 1900;
+
+// // if (number === '' || isNaN(Number(number))) {
+// //   console.log('its not a number!');
+// //   process.exit();
+// // }
+
+// fetch(`http://numbersapi.com/${number}/${type}?json`)
+//   .then(response => {
+//     if (response.ok) {
+//       return response.json()
+//     } else {
+//       throw new Error('ohhhh something wrong: ' + response.status)
+//     }
+//   })
+//   .then(response => console.log(response.text))
+//   .catch(err => console.log('error', err));
+
+/// Task[3] - NBP API
+// `http://api.nbp.pl/api/exchangerates/rates/a/${code}/`
+
+const request = require('request');
+const fs = require('fs');
+
+const validCodes = ['usd', 'eur', 'gbp', 'chf'];
+
+const code = process.argv[2];
+
+const isValid = validCodes.find(currency => currency === code) ? true : false;
+
+const url = `http://api.nbp.pl/api/exchangerates/rates/a/${code}/?format=json`
+
+request(url, { json: true }, (err, res, body) => {
+  if (err) {
+    return console.log("błąd: ", err);
+  }
+  if (res.statusCode !== 200) {
+    return console.log('something wrong check currency!!')
+  }
+  const message = `Średni cena ${body.currency} w dniu ${body.rates[0].effectiveDate} wynosi ${body.rates[0].mid} zł`;
+
+  fs.appendFile('currencies.txt', message + '\n', (err) => {
+    console.log('wynik dodany do pliku');
   })
-  .then(response => console.log(response.text))
-  .catch(err => console.log('error', err));
+
+  console.log(message);
+});
